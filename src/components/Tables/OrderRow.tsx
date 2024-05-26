23784dd96fd1a8b78def062a5fd94fb4dcac8d7e
Cenk SARI <cenk@cenksari.com>
2024-05-26 10:25:41 +0100

    orders

---
+++ src/components/Tables/OrderRow.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import Badge from '../Badge/Badge';
+import Checkbox from '../Forms/Checkbox';
+import Dropdown from '../Dropdown/Dropdown';
+import DropdownItem from '../Dropdown/DropdownItem';
+
+import useClickOutside from '../../hooks/useClickOutside';
+
+import { formatCurrency } from '../../utils/Toolbox';
+
+interface IProps {
+  id: string;
+  date: string;
+  user: string;
+  products: number;
+  totalPrice: number;
+  status: string;
+}
+
+const OrderRow = ({ id, date, user, products, totalPrice, status }: IProps): React.JSX.Element => {  
+  const wrapperRef = React.useRef<any>();
+
+  const [dropdown, setDropdown] = React.useState<boolean>(false);
+
+  const [state, setState] = React.useState<boolean>(false);
+
+  useClickOutside(wrapperRef, () => {
+    setDropdown(false);
+  });
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setState(e.target.checked);
+  };
+
+  return (
+    <tr>
+      <td>
+        <Checkbox name={`cb-${id}`} checked={state} onChange={handleCheckboxChange} />
+      </td>
+      <td>{id}</td>
+      <td>{date}</td>
+      <td>{user}</td>
+      <td className='center'>{products}</td>
+      <td className='center'>{formatCurrency(totalPrice)}</td>
+      <td className='center'>
+        <Badge text={status} color='white' />
+      </td>
+      <td ref={wrapperRef} className='flex flex-v-center flex-end flex-gap-small'>
+        <span className='material-symbols-outlined pointer' onClick={() => setDropdown(!dropdown)}>more_vert</span>
+        {dropdown && (
+          <Dropdown color='gray'>
+            <DropdownItem icon='edit' url={`/user/edit/${id}`} text='Edit' />
+            <DropdownItem icon='visibility' url={`/user/profile/${id}`} text='View' />
+            <hr />
+            <DropdownItem icon='delete' url={`/user/delete/${id}`} text='Delete' />
+          </Dropdown>
+        )}
+      </td>
+    </tr>
+  );
+};
+
+export default OrderRow;
--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import Input from '../../components/Forms/Input';
+import Table from '../../components/Tables/Table';
 import Button from '../../components/Forms/Button';
 import Master from '../../components/Layout/Master';
+import Spacer from '../../components/Spacer/Spacer';
+import OrderRow from '../../components/Tables/OrderRow';
 import Heading from '../../components/Headings/Heading';
 import Container from '../../components/Containers/Container';
 import ButtonGroup from '../../components/ButtonGroup/ButtonGroup';
+
+import orders from '../../data/orders.json';
 
 const buttonGroup = [
   {
@@ -54,6 +59,25 @@ const Orders = (): React.JSX.Element => (
       </Heading>
 
       <ButtonGroup items={buttonGroup} />
+
+      <Spacer />
+
+      <Table header={
+        <tr>
+          <th></th>
+          <th>ID</th>
+          <th>Date</th>
+          <th>User</th>
+          <th className='center'>Products</th>
+          <th className='center'>Total price</th>
+          <th className='center'>Status</th>
+          <th></th>
+        </tr>
+      }>
+        {orders && orders.map((order) => (
+          <OrderRow id={order.id} date={order.date} user={order.user} products={order.products} totalPrice={order.totalPrice} status={order.status} />
+        ))}
+      </Table>
     </Container>
   </Master>
 );
